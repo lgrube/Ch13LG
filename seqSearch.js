b7d7b564a356654ec1e80bb959cbd9e02978555d
lgrube <lgv9483@saintjoe.edu>
2015-04-08 12:22:38 -0500

    example 13-1

---
+++ seqSearch.js
@@ -0,0 +1,28 @@
+//luke grube
+//example 13-1
+
+function seqSearch(arr, data) {
+	for (var i = 0; i < arr.length; ++i) {
+		if(arr[i] == data) {
+			return true;
+		}
+	}
+	return false;
+}
+
+var nums = [];
+for (var i = 0; i < 100; +ii) {
+	nums[i] = Math.floor(Math.random() * 101);
+}
+disArr(nums);
+putstr("Enter a number to search for: ");
+var num = parseInt(readline());
+print();
+if (seqSearch(nums, num)) {
+	print(num + " is in the array.");
+}
+else {
+	print(num + " is not in the array.");
+}
+print();
+dispArr(nums);
