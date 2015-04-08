9d8c7e70fcaf6e003898a26f51ae8e21d488427d
lgrube <lgv9483@saintjoe.edu>
2015-04-08 12:46:25 -0500

    code to show the array

--- dispArr.js
+++ dispArr.js
@@ -1,12 +1,12 @@
 function dispArr(arr) {
 	for (var i = 0; i < arr.length; ++i) {
-		putstr(arr[i] + " ");
+		print(arr[i] + " ");
 		if(i % 10 == 9) {
-			putstr("\n");
+			print("\n");
 		}
 	}
-	if(i % 10 ! = 0) {
-		putstr("\n");
+	if(i % 10 != 0) {
+		print("\n");
 	}
 }
 
